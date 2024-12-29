97b1e5407eb2d7658e0d224335e0d320fd7eb2ee
Marko Haataja <marko@botlabs.fi>
2024-12-29 14:56:57 +0200

    Add multiples_of_a_and_b.py
    Add requirements.txt
    
    Update README.md
    
    Add example input file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # multiples_of_a_and_b
+Multiples of A and B
+
+Take input file containing rows of number data, and outputs multiples of numbers below a goal number.
+
+Input file data must be in the following format:
+Example input file content:
+5 8 31
+4 7 20
+
+Output and output file shall be in the following format:
+Example screen output and output file content:
+20:4 7 8 12 14 16 
+31:5 8 10 15 16 20 24 25 30
+
+
+
+
+How to run:
+
+1. Install python (python version used 3.11.4)
+2. Install python requirements: 
+    * optional: use virtualenv
+    * python -m pip install -r requirements.txt
+3. Run the code: 
+    * e.g. 'python multiples_of_a_and_b.py example_input.txt output.txt'
---
+++ multiples_of_a_and_b.py
@@ -0,0 +1,47 @@
+import sys
+import bisect
+import numpy as np
+
+if len(sys.argv) > 3:
+    print("Too many arguments, please follow the example usage: 'python multiples_of_a_and_b.py input.txt output.txt'")
+    sys.exit()
+
+if len(sys.argv) < 3:
+    print("Too few arguments, please follow the example usage: 'python multiples_of_a_and_b.py input.txt output.txt'")
+    sys.exit()
+
+
+def main():
+    all_multiples = []
+    with open(sys.argv[1], 'r') as inputfile:
+        for line in inputfile:
+            # Read line contents to variables
+            a = int(line.split(' ')[0])
+            b = int(line.split(' ')[1])
+            goal = int(line.split(' ')[2])
+
+            # Use numpy arange to get list of multiples of a and b under goal value
+            # Union of a and b lists sorts values and removes duplicates
+            # Remove 0 from the multiples list
+            multiples = np.delete(np.union1d(np.arange(0, goal, a), np.arange(0, goal, b)), 0).tolist()
+    
+            # Create a string from list items
+            # Add goal and colon in front of the string
+            output_line = ' '.join(str(multiple) for multiple in multiples)
+            output_line = f"{goal}:{output_line}"
+            all_multiples.append(output_line)
+            
+        write_output(all_multiples, sys.argv[2])
+
+
+def write_output(data, output_file):
+    data.sort(key=len)
+    with open(output_file, 'a') as file:
+        for line in data:
+            # Print sorted console output and write to output file
+            print(line)
+            file.write(f"{line}\n")
+
+
+if __name__ == '__main__':
+    main()
